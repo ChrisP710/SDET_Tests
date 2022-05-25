509c610a81d67585245f66c6d15ab0bc66442477
Chris Page <chrispage@periodic.is>
2022-05-25 16:20:06 -0400

    [Completed]

--- Ch1/hello.go
+++ Ch1/hello.go
@@ -3,11 +3,34 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+const english = "English"
+const spanish = "Spanish"
+const french = "French"
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", english))
 }
 
-func Hello(name string) string {
-	return englishHelloPrefix + name
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
