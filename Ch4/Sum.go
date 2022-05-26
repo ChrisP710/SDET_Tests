bd00b322a7fb94d85553d3f9107b1a856ca520bf
Chris Page <chrispage@periodic.is>
2022-05-26 11:51:37 -0400

    [Step 1 Complete]

---
+++ Ch4/Sum.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	return
+}
+
+func Sum(numbers []int) int {
+	sum := 0
+
+	// for i := 0; i < 5; i++ {
+	// 	sum += numbers[i]
+	// }
+
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
+}
