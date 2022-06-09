f14b88e639e1b9ec6f5782d20ce2352c71ec99e8
Chris Page <chrispage@periodic.is>
2022-06-09 12:04:23 -0400

    adding reflection test

--- Ch12/reflection_test.go
+++ Ch12/reflection_test.go
@@ -1,5 +1,47 @@
 package reflection
 
 import (
+	"reflect"
 	"testing"
 )
+
+func TestWalk(t *testing.T) {
+
+	type Profile struct {
+		Age  int
+		City string
+	}
+
+	type Person struct {
+		Name    string
+		Profile Profile
+	}
+
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Pointer to things",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
